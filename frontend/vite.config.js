70f76a896ae2faba26d53982cde8033fe9577986
Ryan Putka <rputka@nd.edu>
2026-05-06 00:13:51 -0400

    last change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
+    port: 5173,
     strictPort: true,
     proxy: {
       "/api": "http://localhost:8000",
